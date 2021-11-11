86f5a70b67cfbaf09a6887af3dedae267207431e
datnt19213 <91647943+datnt19213@users.noreply.github.com>
2021-11-11 17:30:50 +0700

    Update Management.php

--- Management.php
+++ Management.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Management</title>
     <link rel="stylesheet" href="css/management.css">
-    <script src="js/\/management.js"></script>
+    <script src="js/management.js"></script>
 </head>
 <body>
     <div id="wrapper">
