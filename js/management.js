bfbc357bc24884f77f122e0e72e7b4fb6d767f1d
datnt19213 <91647943+datnt19213@users.noreply.github.com>
2021-11-11 17:25:54 +0700

    Update management.js

--- js/management.js
+++ js/management.js
@@ -4,23 +4,61 @@ function choice(x){
         document.getElementById('delete').style.display='none';
         document.getElementById('update').style.display='none';
         document.getElementById('add').style.display='block';
+        document.getElementById('add-pub').style.display='none';
+        document.getElementById('add-cate').style.display='none';
+        document.getElementById('add-shop').style.display='none';
     }
     else if(x==1){
         document.getElementById('sta').style.display='none';
         document.getElementById('delete').style.display='none';
         document.getElementById('update').style.display='block';
         document.getElementById('add').style.display='none';
+        document.getElementById('add-pub').style.display='none';
+        document.getElementById('add-cate').style.display='none';
+        document.getElementById('add-shop').style.display='none';
     }
     else if(x==2){
         document.getElementById('sta').style.display='none';
         document.getElementById('delete').style.display='block';
         document.getElementById('update').style.display='none';
         document.getElementById('add').style.display='none';
+        document.getElementById('add-pub').style.display='none';
+        document.getElementById('add-cate').style.display='none';
+        document.getElementById('add-shop').style.display='none';
+    }
+    else if(x==3){
+        document.getElementById('sta').style.display='none';
+        document.getElementById('delete').style.display='none';
+        document.getElementById('update').style.display='none';
+        document.getElementById('add').style.display='none';
+        document.getElementById('add-pub').style.display='block';
+        document.getElementById('add-cate').style.display='none';
+        document.getElementById('add-shop').style.display='none';
+    }else if(x==4){
+        document.getElementById('sta').style.display='none';
+        document.getElementById('delete').style.display='none';
+        document.getElementById('update').style.display='none';
+        document.getElementById('add').style.display='none';
+        document.getElementById('add-pub').style.display='none';
+        document.getElementById('add-cate').style.display='block';
+        document.getElementById('add-shop').style.display='none';
+    }
+    else if(x==5){
+        document.getElementById('sta').style.display='none';
+        document.getElementById('delete').style.display='none';
+        document.getElementById('update').style.display='none';
+        document.getElementById('add').style.display='none';
+        document.getElementById('add-pub').style.display='none';
+        document.getElementById('add-cate').style.display='none';
+        document.getElementById('add-shop').style.display='block';
     }
     else{
         document.getElementById('sta').style.display='block';
         document.getElementById('delete').style.display='none';
         document.getElementById('update').style.display='none';
         document.getElementById('add').style.display='none';
+        document.getElementById('add-pub').style.display='none';
+        document.getElementById('add-cate').style.display='none';
+        document.getElementById('add-shop').style.display='none';
     }
 }
