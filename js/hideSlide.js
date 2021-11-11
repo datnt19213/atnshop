cd98277e242f7805361d9e79dbab0aa5bcaeff36
datnt19213 <91647943+datnt19213@users.noreply.github.com>
2021-11-11 16:21:34 +0700

    Add files via upload

---
+++ js/hideSlide.js
@@ -0,0 +1,8 @@
+function hide(x){
+    if(x==0 || x==1){
+        document.getElementById('card').style.display='none';
+    }
+    else{
+        document.getElementById('signup').style.display='block';
+    }
+}
